83becbfa1cef47aefe5b45cfb00372e467fd5e69
DeViL8077 <mann6657@gmail.com>
2026-07-20 23:53:08 +0530

    setup initial FastAPI server, jinja2templates, and directory scanner

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import os
-app=FastAPI()
-@app.get("/")
-def root():
-    return {"message": "NexusShare is live!"}
 
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
 @app.get("/files")
 def file_lists():
     folder_path="shared files"
